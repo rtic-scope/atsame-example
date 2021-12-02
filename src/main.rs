d478ff8ce57618f73602ae73189d068be9d773c1
Henrik Tjäder <henrik.tjader@volvocars.com>
2021-12-02 10:51:28 +0100

    AES: Demo using AES encryption

--- src/main.rs
+++ src/main.rs
@@ -4,27 +4,27 @@
 
 macro_rules! clear_screen {
     ($tx:tt) => {{
-    // Sequence:
-    // `<ESC>[2J` (clear screen)
-    // `<ESC>[H` (cursor to home position)
-    // `<ESC>` == 0x1b
-    $tx.write_str("\x1b").unwrap();
-    $tx.write_str("[2J").unwrap();
-    $tx.write_str("\x1b").unwrap();
-    $tx.write_str("[H").unwrap();
-    }}
+        // Sequence:
+        // `<ESC>[2J` (clear screen)
+        // `<ESC>[H` (cursor to home position)
+        // `<ESC>` == 0x1b
+        $tx.write_str("\x1b").unwrap();
+        $tx.write_str("[2J").unwrap();
+        $tx.write_str("\x1b").unwrap();
+        $tx.write_str("[H").unwrap();
+    }};
 }
 
 macro_rules! clear_line {
     ($tx:tt) => {{
-    // Sequence:
-    // `<ESC>[2K` (clear line)
-    // `\r`: CR
-    // `<ESC>` == 0x1b
-    $tx.write_str("\x1b").unwrap();
-    $tx.write_str("[2K").unwrap();
-    $tx.write_str("\r").unwrap();
-    }}
+        // Sequence:
+        // `<ESC>[2K` (clear line)
+        // `\r`: CR
+        // `<ESC>` == 0x1b
+        $tx.write_str("\x1b").unwrap();
+        $tx.write_str("[2K").unwrap();
+        $tx.write_str("\r").unwrap();
+    }};
 }
 
 #[panic_handler]
@@ -41,7 +41,11 @@ fn panic(info: &core::panic::PanicInfo) -> ! {
 
 use core::fmt::Write as _;
 
+// Software Aes192
+//use aes::Aes192;
+
 use atsamd_hal::{
+    aes::*,
     clock::GenericClockController,
     dsu::Dsu,
     gpio::v2::{Alternate, Pin, Pins, D, PA04, PA05},
@@ -51,6 +55,7 @@ use atsamd_hal::{
     sercom::v2::{uart::*, IoSet3, Sercom0},
     time::U32Ext,
 };
+
 use nb::block;
 
 use rtic::app;
@@ -97,7 +102,7 @@ mod app {
         let gclk0 = clocks.gclk0();
 
         let mut uart0 = Config::new(
-            &mclk,
+            mclk,
             device.SERCOM0,
             Pads::default().rx(pins.pa05).tx(pins.pa04),
             clocks.sercom0_core(&gclk0).unwrap().freq(),
@@ -108,6 +113,165 @@ mod app {
 
         let nvm = Nvm::new(device.NVMCTRL);
         let dsu = Dsu::new(device.DSU, &device.PAC).unwrap();
+
+        // Enable bus clocking for AES peripheral
+        mclk.apbcmask.modify(|_, w| w.aes_().set_bit());
+
+        let key = [
+            0x54, 0x68, 0x61, 0x74, 0x73, 0x20, 0x6D, 0x79, 0x20, 0x4B, 0x75, 0x6E, 0x67, 0x20,
+            0x46, 0x75,
+        ];
+
+        let message = [
+            0x54, 0x77, 0x6F, 0x20, 0x4F, 0x6E, 0x65, 0x20, 0x4E, 0x69, 0x6E, 0x65, 0x20, 0x54,
+            0x77, 0x6F,
+        ];
+
+        let ciphertext = *aes::Block::from_slice(&[
+            0x29, 0xC3, 0x50, 0x5F, 0x57, 0x14, 0x20, 0xF6, 0x40, 0x22, 0x99, 0xB3, 0x1A, 0x02,
+            0xD7, 0x3A,
+        ]);
+
+        let aeskey = GenericArray::from_slice(&key);
+        let mut aesmsg = *aes::Block::from_slice(&message);
+
+        // Atsamd peripheral init
+        let aes = Aes::new(device.AES);
+        let aes_reg = aes.destroy();
+
+        // Store AES hardware peripheral in AesRustCrypto to ensure
+        // no uses of the AES peripheral outside RustCrypto
+        let aes_rust_crypto = Aes::new_rustcrypto_backend(aes_reg);
+
+        let aes128 = aes_rust_crypto.new_128bit(aeskey);
+        // Direct access to AES peripheral through RustCrypto
+        //let aes128 = Aes128::new(aeskey);
+
+        write!(
+            &mut uart0 as &mut dyn Write<_, Error = _>,
+            "Begin AES demo\r\n"
+        )
+        .unwrap();
+
+        for x in aesmsg {
+            write!(&mut uart0 as &mut dyn Write<_, Error = _>, "{}", x as char).unwrap();
+        }
+        write!(&mut uart0 as &mut dyn Write<_, Error = _>, "\r\n").unwrap();
+        for x in aesmsg {
+            write!(&mut uart0 as &mut dyn Write<_, Error = _>, "{:#04x} ", x).unwrap();
+        }
+        write!(&mut uart0 as &mut dyn Write<_, Error = _>, "\r\n").unwrap();
+
+        // Encrypt message
+        aes128.encrypt_block(&mut aesmsg);
+
+        for x in aesmsg {
+            write!(&mut uart0 as &mut dyn Write<_, Error = _>, "{}", x as char).unwrap();
+        }
+        write!(&mut uart0 as &mut dyn Write<_, Error = _>, "\r\n").unwrap();
+        for x in aesmsg {
+            write!(&mut uart0 as &mut dyn Write<_, Error = _>, "{:#04x} ", x).unwrap();
+        }
+        write!(
+            &mut uart0 as &mut dyn Write<_, Error = _>,
+            "   <- AES ciphertext\r\n"
+        )
+        .unwrap();
+        for x in ciphertext {
+            write!(&mut uart0 as &mut dyn Write<_, Error = _>, "{:#04x} ", x).unwrap();
+        }
+        write!(
+            &mut uart0 as &mut dyn Write<_, Error = _>,
+            "   <- known ciphertext\r\n"
+        )
+        .unwrap();
+
+        assert_eq!(aesmsg, ciphertext);
+
+        // Decrypt message
+        aes128.decrypt_block(&mut aesmsg);
+
+        for x in aesmsg {
+            write!(&mut uart0 as &mut dyn Write<_, Error = _>, "{}", x as char).unwrap();
+        }
+        write!(&mut uart0 as &mut dyn Write<_, Error = _>, "\r\n").unwrap();
+        for x in aesmsg {
+            write!(&mut uart0 as &mut dyn Write<_, Error = _>, "{:#04x} ", x).unwrap();
+        }
+        write!(&mut uart0 as &mut dyn Write<_, Error = _>, "\r\n").unwrap();
+
+        cortex_m::asm::bkpt();
+
+        // AES RustCrypto Example
+
+        let key = GenericArray::from_slice(&[0u8; 16]);
+        let mut block = aes::Block::default();
+
+        // Initialize cipher
+        let cipher = atsamd_hal::aes::Aes128::new(key);
+
+        let block_copy = block;
+
+        // Encrypt block in-place
+        cipher.encrypt_block(&mut block);
+
+        // And decrypt it back
+        cipher.decrypt_block(&mut block);
+        assert_eq!(block, block_copy);
+
+        cortex_m::asm::bkpt();
+
+        // AES CMAC RustCrypto Example
+
+        use cmac::{Cmac, Mac, NewMac};
+
+        // Create `Mac` trait implementation, namely CMAC-AES128
+        let mut mac = Cmac::<Aes128>::new_from_slice(b"very secret key.").unwrap();
+        mac.update(b"input message");
+
+        // `result` has type `Output` which is a thin wrapper around array of
+        // bytes for providing constant time equality check
+        let result = mac.finalize();
+        // To get underlying array use the `into_bytes` method, but be careful,
+        // since incorrect use of the tag value may permit timing attacks which
+        // defeat the security provided by the `Output` wrapper
+        let tag_bytes = result.into_bytes();
+
+        //To verify the message:
+
+        let mut mac = Cmac::<Aes128>::new_from_slice(b"very secret key.").unwrap();
+
+        mac.update(b"input message");
+
+        // `verify` will return `Ok(())` if tag is correct, `Err(MacError)` otherwise
+        mac.verify(&tag_bytes).unwrap();
+        cortex_m::asm::bkpt();
+
+        // AES Counter RustCrypto Example
+        use ctr::cipher::{NewCipher, StreamCipher, StreamCipherSeek};
+
+        // `aes` crate provides AES block cipher implementation
+        type Aes128Ctr = ctr::Ctr128BE<atsamd_hal::aes::Aes128>;
+
+        let mut data = [1, 2, 3, 4, 5, 6, 7];
+
+        let key = b"very secret key.";
+        let nonce = b"and secret nonce";
+
+        // create cipher instance
+        let mut cipher = Aes128Ctr::new(key.into(), nonce.into());
+
+        // apply keystream (encrypt)
+        cipher.apply_keystream(&mut data);
+        assert_eq!(data, [6, 245, 126, 124, 180, 146, 37]);
+
+        // seek to the keystream beginning and apply it again to the `data` (decrypt)
+        cipher.seek(0);
+        cipher.apply_keystream(&mut data);
+        assert_eq!(data, [1, 2, 3, 4, 5, 6, 7]);
+
+        cortex_m::asm::bkpt();
+
         write!(
             &mut uart0 as &mut dyn Write<_, Error = _>,
             "RTIC booted!\r\n"
