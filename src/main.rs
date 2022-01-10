fc4ce8c166454bcf5ac9f9ac2b71b9b91bdf95df
Viktor Sonesten <viktor.sonesten@grepit.se>
2022-01-10 16:26:20 +0100

    configure tracing with RTIC Scope

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,24 @@ mod app {
     fn init(mut ctx: init::Context) -> (SharedResources, LocalResources, init::Monotonics()) {
         rtic::pend(Interrupt::EIC_EXTINT_0);
 
+        cortex_m_rtic_trace::configure(
+            &mut ctx.core.DCB,
+            &mut ctx.core.TPIU,
+            &mut ctx.core.DWT,
+            &mut ctx.core.ITM,
+            1, // task enter DWT comparator ID
+            2, // task exit DWT comparator ID
+            &TraceConfiguration {
+                delta_timestamps: LocalTimestampOptions::Enabled,
+                absolute_timestamps: GlobalTimestampOptions::Disabled,
+                timestamp_clk_src: TimestampClkSrc::AsyncTPIU,
+                tpiu_freq: 16_000_000, // Hz
+                tpiu_baud: 115_200,    // B/s
+                protocol: TraceProtocol::AsyncSWONRZ,
+            },
+        )
+            .unwrap();
+
         (SharedResources {}, LocalResources {}, init::Monotonics())
     }
 
